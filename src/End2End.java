7b44d0049198b81780c654e0c2872e55b521aa37
Willowviu <willowviu3@gmail.com>
2025-01-08 10:59:14 +0100

    I've translated to English some lines

--- src/End2End.java
+++ src/End2End.java
@@ -20,39 +20,36 @@ public class End2End {
 		driver.manage().deleteAllCookies();
 		driver.get("https://www.iberia.com");
 
-		// Espera explícita
+		// Explicit Wait
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-		// Rechazar cookies
+		// Refuse cookies
 		WebElement rejectCookiesButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("onetrust-reject-all-handler")));
 		rejectCookiesButton.click();
 
-		// Hacer click en pagar with avios
-        //driver.findElement(By.id("paywithAvios")).click();
-        		
-		// Ingresar origen 
+		// Insert origin 
 		 WebElement flightOrigin = driver.findElement(By.id("flight_origin1"));
 		 flightOrigin.sendKeys("Madrid");
 		
-		// Ingresar destino
+		// Insert destination
 		WebElement flightDestiny = driver.findElement(By.id("flight_destiny1"));
 		flightDestiny.sendKeys("Ibiza");
 		
 		Thread.sleep(1000);
 
-		// Abrir el calendario para seleccionar la fecha de ida 
+		// Open the calendar to select the departure date
 		WebElement date1Label = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='flight_round_date1']")));
 		date1Label.click();
 
-		// Seleccionar fecha de ida (dia 23)
+		// Select the departure date (23rd)
 		WebElement date23 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='searcher-calendar-1']//a[text()='23']")));
 	    date23.click();
 
-	    // Seleccionar fecha de vuelta (día 27)
+	    // Select the return date (27th)
         WebElement date27 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='searcher-calendar-end-1']//a[text()='27']")));
         date27.click();
         
-        // Seleccionar 5 adulto
+        // Select 5 adults
         driver.findElement(By.id("flight_passengers1")).click();
         for(int i=1;i<5;i++)
         {	
@@ -61,14 +58,14 @@ public class End2End {
         		+ "")).click();
         }       
         
-        // Seleccionar pagar con avios
+        // Select "pay with avios"
         driver.findElement(By.xpath("//div[@class='ibe-form__checkbox inte-avioscheck']")).click();
         
-        // Confirmar y buscar
+        // Confirm and search
         WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[class='ibe-button__text']")));
         searchButton.click();
 
-        System.out.println("Has completado tu compra con éxito Lu mi amoggg.");
+        System.out.println("Has completado tu compra con éxito.");
         //driver.quit();
     }
 	

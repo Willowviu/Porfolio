1108bad890555de7ecc7e6b8f8009bba0e17748b
Willowviu <willowviu3@gmail.com>
2025-01-23 10:01:02 +0100

    adding proxy and setting download directory

--- src/SSLCheck.java
+++ src/SSLCheck.java
@@ -3,6 +3,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 
 
@@ -13,6 +18,13 @@ public class SSLCheck {
 
 		ChromeOptions options = new ChromeOptions();
 		options.setAcceptInsecureCerts(true);
+		Proxy proxy = new Proxy();
+		proxy.setHttpProxy("ipaddress:4444");
+		options.setCapability("proxy", proxy);
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("download.default_directory", "/directory/path");
+		options.setExperimentalOption("prefs", prefs);
+		
 		//FirefoxOptions options2 = new FirefoxOptions();
 		//options2.setAcceptInsecureCerts(true);
 		//EdgeOptions options3 = new EdgeOptions();

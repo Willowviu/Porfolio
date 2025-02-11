594ad0dbdd63d424d2509da91b4b23902f71e9a4
Willowviu <willowviu3@gmail.com>
2025-02-11 09:00:28 +0100

    updated NewWindow.java

--- src/NewWindow.java
+++ src/NewWindow.java
@@ -1,16 +1,21 @@
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 
 
 public class NewWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
 		WebDriver driver = new FirefoxDriver();
@@ -26,8 +31,10 @@ public class NewWindow {
 		String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p'] "))
 		.get(1).getText();
 		driver.switchTo().window(parentWindowId);
-		driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseName);
+		WebElement name = driver.findElement(By.cssSelector("[name='name']"));
+		name.sendKeys(courseName);
+		File file = name.getScreenshotAs(OutputType.FILE);
 		
-	}
-
+		FileUtils.copyFile(file, new File("logo.png"));
+}
 }
